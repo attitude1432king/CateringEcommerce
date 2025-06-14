7b4e03761a007109660f1489a342a42bfd65b70e
attitude1432king <110887294+attitude1432king@users.noreply.github.com>
2025-06-14 20:17:08 +0530

    Recommit for Home & React Frontend

--- CateringEcommerce.BAL/Configuration/TwilioSettings.cs
+++ CateringEcommerce.BAL/Configuration/TwilioSettings.cs
@@ -1,9 +1,9 @@
 ﻿namespace CateringECommerce.BAL.Configuration
 {
-    public static class TwilioSettings
+    public class TwilioSettings
     {
-        public static readonly string AccountSid = "";
-        public static readonly string AuthToken = "";
+        public string AccountSid { get; set; } = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        public static readonly string AuthToken = "32d72da8576aab7007b38891731caf34";
         public static readonly string FromPhoneNumber = "81601 82327";
     }
 }
