135723b88222c1e3c63cffc9e633f60954275fd7
attitude1432king <110887294+attitude1432king@users.noreply.github.com>
2025-06-14 19:27:38 +0530

    React + vite & Home page commit again

--- CateringEcommerce.BAL/Configuration/TwilioSettings.cs
+++ CateringEcommerce.BAL/Configuration/TwilioSettings.cs
@@ -2,8 +2,8 @@
 {
     public static class TwilioSettings
     {
-        public static readonly string AccountSid = "ACa0e64157a3eeacc75d11d3ca0a45dc58";
-        public static readonly string AuthToken = "32d72da8576aab7007b38891731caf34";
+        public static readonly string AccountSid = "";
+        public static readonly string AuthToken = "";
         public static readonly string FromPhoneNumber = "81601 82327";
     }
 }
