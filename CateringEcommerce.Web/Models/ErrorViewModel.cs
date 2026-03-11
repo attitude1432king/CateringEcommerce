86f3939654f57c733cb5df690d85c7be40b08795
attitude1432king <pan123lohar@gmail.com>
2026-03-12 01:52:38 +0530

    Deleting thje Frontend folder and Web projects for ASP.NET Core related

--- CateringEcommerce.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CateringEcommerce.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
